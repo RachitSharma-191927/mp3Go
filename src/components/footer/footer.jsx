7bc17f5bfa90f9c0621b797cd46df51495cff5b8
rachit-almabetter <133861502+rachit-almabetter@users.noreply.github.com>
2023-10-30 00:27:56 +0530

    Fully Done

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -50,8 +50,7 @@ export default function Example() {
             variant="small"
             className="mb-4 text-center font-normal text-blue-gray-900 md:mb-0 text-white">
             &copy; {currentYear}{" "}
-            <a href="https://material-tailwind.com/">Material Tailwind</a>. All
-            Rights Reserved.
+            <a href="https://material-tailwind.com/">Rachit Sharma</a>
           </Typography>
           <div className="flex gap-4 text-blue-gray-900 sm:justify-center">
             <Typography
